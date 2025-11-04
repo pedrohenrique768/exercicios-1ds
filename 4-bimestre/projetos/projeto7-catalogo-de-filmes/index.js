97873365b5c01a803fd77e81a33043dddd173d37
Pedro Henrique <pedro.castro109@aluno.ce.gov.br>
2025-11-04 14:21:29 +0000

    concluido

--- 4-bimestre/projetos/projeto7-catalogo-de-filmes/index.js
+++ 4-bimestre/projetos/projeto7-catalogo-de-filmes/index.js
@@ -1,4 +1,28 @@
 // CRIE UMA LÓGICA ABAIXO
 
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
+
+//catalogo de filmes//
+let catalogo = [
+ filme1 = "invocação do mal",
+ filme2 = "a freira",
+ filme3 = "anabelle",
+ filme4 = "a maldição da chorona",
+ filme5 = "sorria",
+ filme6 = "pânico",
+ filme7 = "telefone preto",
+ filme8 = "it a coisa",
+ filme9 = "premonição",
+ filme10 = "evil dead",
+
+]
+
+
 module.exports = { filme1, filme2, filme3, filme4, filme5, filme6, filme7, filme8, filme9, filme10 }
+
+
+
+
+
+
+
